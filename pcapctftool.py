3d8c8594dd1cd0d4cfdd67f51c656b74ba448513
W1nterFr3ak <byronchris25@gmail.com>
2023-03-24 11:52:08 +0300

    pcapctftool: credential seacrh faulty

--- pcapctftool.py
+++ pcapctftool.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-
+import re
 import sys
 import argparse
 import logging
@@ -21,11 +21,14 @@ from scapy.layers import http
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 # open file
-def open_pcap(inputfile):
+def open_pcap(inputfile, f=None):
     print('\n') 
     cprint(f"[+] Load PCAP.....", 'blue')
     try: 
-        packets = sniff(offline=inputfile, session=TCPSession)
+        if f:
+            packets = sniff(offline=inputfile, filter=f, session=TCPSession)
+        else:
+            packets = sniff(offline=inputfile, session=TCPSession)
         cprint(f"[-] {inputfile} loaded\n",'green')  
     except:
         cprint(f"[!]File: {inputfile} loading failed\n", 'red')
@@ -33,5 +36,24 @@ def open_pcap(inputfile):
     return packets
 
 
-if __name__ == '__main__';
-    open_pcap(sys.argv[1])
+def cred_search(inputfile):
+    # frame matches "(?i)(passwd|pass|password)"
+    pattern = b"(?i)(pass(wor)?d.*|user(name)?.*)"
+    print('\n')     
+    filt = 'frame matches "(?i)(passwd|pass|password)"'
+    packets = open_pcap(inputfile)
+    cprint(f"[+] Cred Search.....", 'blue')
+    
+    for packet in packets:
+        if packet.haslayer('Raw'):
+            payload = packet.getlayer('Raw').load
+            if re.search(pattern,payload):
+                try:
+                    cprint(payload.decode('utf-8'), 'green')
+                except UnicodeDecodeError:
+                    cprint(payload.decode('utf-8', 'ignore'), 'green')
+                    
+    # return packets
+
+if __name__ == '__main__':
+    cred_search(sys.argv[1])
